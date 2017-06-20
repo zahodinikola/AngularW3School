e91a6ade58213997bd9ef0fe907a4cf6969b96ae
zahodinikola <zahodinikola@gmail.com>
2017-06-20 17:51:45 +0300

    services

---
+++ js/controllers/namesCtrl.js
@@ -0,0 +1,7 @@
+app.controller('namesCtrl', function($scope) {
+	$scope.names = [
+	{name: "Bob", country: "USA"},
+	{name: "Lola", country: "Bolivia"},
+	{name: "Viktor", country: "Namibia"}
+	];
+});
